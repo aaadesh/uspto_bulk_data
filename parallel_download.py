8d3eeb3a7eb64f397ab67c142c2417e5efc08df0
Aadesh Srivastava <118649492+aaadesh@users.noreply.github.com>
2023-10-29 15:25:19 +0530

    Update parallel_download.py

--- parallel_download.py
+++ parallel_download.py
@@ -10,40 +10,51 @@ from tqdm import tqdm
 from concurrent.futures import ThreadPoolExecutor
 
 def download_zip_file(url, save_path):
-    chunk_size = 1024  # Adjust the chunk size as needed
-    with requests.get(url, stream=True) as response:
-        total_size = int(response.headers.get('content-length', 0))
-        progress_bar = tqdm(total=total_size, unit='B', unit_scale=True, desc=f"Downloading {os.path.basename(save_path)}", position=0, leave=True)
+    chunk_size = 5000
 
-        with open(save_path, 'wb') as file, ThreadPoolExecutor(max_workers=4) as executor:
+    if os.path.exists(save_path):
+        local_size = os.path.getsize(save_path)
+        headers = {'Range': f'bytes={local_size}-'}
+        mode = 'ab'
+    else:
+        local_size = 0
+        headers = None
+        mode = 'wb'
+
+    with requests.get(url, stream=True, headers=headers) as response:
+        if response.status_code == 206:
+            print(f"Resuming download of {os.path.basename(save_path)}")
+        else:
+            print(f"Downloading {os.path.basename(save_path)}")
+
+        total_size = int(response.headers.get('content-length', 0)) + local_size
+        progress_bar = tqdm(total=total_size, initial=local_size, unit='B', unit_scale=True, desc=f"Downloading {os.path.basename(save_path)}", position=0, leave=True)
+
+        with open(save_path, mode) as file, ThreadPoolExecutor(max_workers=4) as executor:
             futures = []
 
             for chunk in response.iter_content(chunk_size=chunk_size):
                 if chunk:
-                    # Write the chunk to the file
                     futures.append(executor.submit(file.write, chunk))
-                    # Update the progress bar
                     progress_bar.update(len(chunk))
 
-            # Wait for all threads to finish
             for future in futures:
                 future.result()
 
-    # Close the progress bar
-    progress_bar.close()
+        progress_bar.close()
+
 
 
 
 def get_patent_data(year):
 
-	baseurl = "https://bulkdata.uspto.gov/data/patent/officialgazette/"
+	baseurl = "https://bulkdata.uspto.gov/data/patent/grant/redbook/fulltext/"
 	url1 = urljoin(baseurl, str(year))
 	print(url1)
 
 	r = requests.get(url1)
 	datas = r.content
 
-	# Check if the request was successful (status code 200)
 	if r.status_code == 200:
 		print("accessing data")
 
@@ -63,7 +74,7 @@ def get_patent_data(year):
 		link_parts = links.split('/')
 		filename = link_parts[-1]
 		filename = os.path.basename(filename)
-		print(filename)
+		#print(filename)
 		save_path = os.path.join(save_dir, filename)
 		download_zip_file(links, save_path)
 
